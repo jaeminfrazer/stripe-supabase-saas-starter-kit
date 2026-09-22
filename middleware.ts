f1435807ae0d4cd4929e3b0ed96bbb22806e6041
jaeminfrazer <jaemin@jaeminfrazer.com>
2026-09-22 17:35:13 +1000

    Update request path matcher to exclude API routes

--- middleware.ts
+++ middleware.ts
@@ -8,13 +8,13 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
+         * Match all request paths except for:
+         * - /api routes
+         * - _next/static
+         * - _next/image
+         * - favicon.ico
+         * - image files
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
-
