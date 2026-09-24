a2c27c8b4645114c1554d409edaccf85f401a880
jaeminfrazer <jaemin@jaeminfrazer.com>
2026-09-25 09:32:47 +1000

    Refactor DashboardHeader component and remove unused imports

--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -1,21 +1,14 @@
-import { Bell, Menu, Search } from "lucide-react"
+import { Menu, Search } from "lucide-react"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import Image from 'next/image'
-import { createClient } from '@/utils/supabase/server'
+import Image from "next/image"
 import DashboardHeaderProfileDropdown from "./DashboardHeaderProfileDropdown"
-import { Badge } from "@/components/ui/badge"
-import { getStripePlan } from "@/utils/stripe/api"
-import { Suspense } from "react"
-import { Skeleton } from "@/components/ui/skeleton"
 
 export default async function DashboardHeader() {
     const supabase = createClient()
-    const { data: { user }, error } = await supabase.auth.getUser()
-    // Get the user's plan from Stripe
-    const stripePlan = getStripePlan(user!.email!)
+    const { data: { user } } = await supabase.auth.getUser()
 
     return (
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -24,9 +17,7 @@ export default async function DashboardHeader() {
                     <Link className="mr-2 flex items-center space-x-2" href="">
                         <Image src="/logo.png" alt="logo" width={25} height={25} />
                     </Link>
-                    <Suspense fallback={<Badge variant="outline" className="mr-2"><Skeleton className="w-[50px] h-[20px] rounded-full" /></Badge>}>
-                        <Badge variant="outline" className="mr-2">{stripePlan}</Badge>
-                    </Suspense>
+
                     <nav className="flex items-center space-x-6 text-sm font-medium">
                         <Link className="transition-colors hover:text-foreground/80 text-foreground" href="#">
                             Home
@@ -45,10 +36,12 @@ export default async function DashboardHeader() {
                         </Link>
                     </nav>
                 </div>
+
                 <Button variant="outline" size="icon" className="mr-2 md:hidden">
                     <Menu className="h-5 w-5" />
                     <span className="sr-only">Toggle Menu</span>
                 </Button>
+
                 <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
                     <div className="w-full flex-1 md:w-auto md:flex-none">
                         <form>
@@ -62,6 +55,7 @@ export default async function DashboardHeader() {
                             </div>
                         </form>
                     </div>
+
                     <DashboardHeaderProfileDropdown />
                 </div>
             </div>
